af1c2d5e03a70ee96eff84972cbfa6d50e0a3990
aidabehmard <aida.behmard@yale.edu>
2014-03-17 18:40:20 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ from visual import *
 from visual.graph import *
 
 sun = RigidSphere(6.955e8, 24*86400, [1,1,1])
-#spots = [sun.spot(0,1,4e7)]
+spots = [sun.spot(0,1,4e7)]
 sim = Simulation(sun, 200, spots)
 rgb = sim.render(400000)
 
