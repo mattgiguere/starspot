da1bc6385b5fb086705f614afaa33f62543598c4
Cyril Zhang <cyril@mentalcanvas.com>
2014-03-28 13:13:07 -0400

    flesh out spec demo

--- spec.py
+++ spec.py
@@ -3,11 +3,12 @@
 import numpy as np
 from starspot import *
 
-sun = RigidSphere(6.955e8, 24*86400, [0,1,1])
-spots = [sun.spot(0,1,4e7)]
-sim = Simulation(sun, 50, spots)
+sun = RigidSphere(6.955e8, 20*86400, [0,1,1])
+spots = [sun.spot(0,1,8e7)]
+sim = Simulation(sun, 100, spots)
 
-def render_time(t):
+# render at a time
+def rt(t):
     rgb = sim.render(t)
 
     plt.clf()
@@ -17,9 +18,21 @@ def render_time(t):
     plt.imshow(rgb, interpolation='nearest')
 
     plt.subplot(1,2,2)
-    x = np.linspace(0,1,100)
+    x = np.linspace(0,1,1000)
     base = physics.voigt(x, 1, 0.5, 0.1, 0)
-    plt.plot(x, base, label='base')
-    plt.legend( ('base') )
+    plt.plot(x, base, label='base', color='gray')
 
+    ret = np.zeros( base.shape )
+    mask = sim.compute_mask(t)
+    for rv, m in zip(sim.radvels, mask):
+        ret += m * physics.voigt(x + rv/10000.0, 1, 0.5, 0.1, 0)
+    ret /= mask.sum()
+    plt.plot(x, ret, label='spread', color='red')
+
+    plt.axvline(0.5, color='gray')
+    plt.axvline(x[np.argmax(ret)], color='red')
+
+    plt.legend( ('base', 'spread') )
     plt.show()
+
+rt(0)
--- starspot/geometry.py
+++ starspot/geometry.py
@@ -9,7 +9,7 @@ def rotation_matrix(angle, direction):
     R = np.diag([cosa, cosa, cosa])
     R += np.outer(direction, direction) * (1.0 - cosa)
     d = direction * sina
-    R += np.array([[ 0.0,         -d[2],  d[1]],
-                   [ d[2], 0.0,          -d[0]],
-                   [-d[1], d[0],  0.0]])
+    R += np.array([[ 0.0, -d[2], d[1]],
+                   [ d[2], 0.0, -d[0]],
+                   [-d[1], d[0], 0.0]])
     return R
