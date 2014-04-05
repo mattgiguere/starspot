0e5895fb4e58af2f2a896a86fe72d8a8bf2facc1
Cyril Zhang <cyril@mentalcanvas.com>
2014-04-05 04:15:59 -0400

    time series simulation done

--- spec.py
+++ spec.py
@@ -3,21 +3,19 @@
 import numpy as np
 from starspot import *
 
-sun = RigidSphere(6.955e8, 20*86400, [0,1,1])
-spots = [sun.spot(0,1,8e7)]
-sim = Simulation(sun, 100, spots)
+sun = RigidSphere(6.955e8, 20*86400, [0,1,0])
+spots = [sun.spot(0,1,20e7)]
+sim = Simulation(sun, 50, spots)
 
 # render at a time
 def rt(t):
     rgb = sim.render(t)
 
-    plt.clf()
-
-    plt.subplot(1,2,1)
+    plt.subplot(3,1,1)
     plt.cla()
     plt.imshow(rgb, interpolation='nearest')
 
-    plt.subplot(1,2,2)
+    plt.subplot(3,1,2)
     x = np.linspace(0,1,1000)
     base = physics.voigt(x, 1, 0.5, 0.1, 0)
     plt.plot(x, base, label='base', color='gray')
@@ -33,6 +31,20 @@ def rt(t):
     plt.axvline(x[np.argmax(ret)], color='red')
 
     plt.legend( ('base', 'spread') )
-    plt.show()
 
+    return np.average(x, weights=ret)
+
+plt.clf()
 rt(0)
+
+T = np.linspace(0,2000000,100)
+rv = []
+for t in T:
+    print t
+    rv.append(rt(t))
+
+RV = np.array(rv)
+
+plt.subplot(3,1,3)
+plt.plot(T,RV)
+plt.show()
