4ef38fbda96bc0ca87d6e5e36e0789572e000156
Cyril Zhang <cyril@mentalcanvas.com>
2014-04-14 03:58:32 -0400

    modify spectrum demo to show off curve smoothness

--- spec.py
+++ spec.py
@@ -6,26 +6,21 @@ from starspot.geometry import inclined_axis
 
 period = 20*86400
 sun = RigidSphere(6.955e8, period, [0,1,2])
-spots = [sun.spot(45,0.5,0.01)]
+spots = [sun.spot(60,0.5,0.001)]
 
-rt = Raytracer(sun, spots, 100)
+rt = Raytracer(sun, spots, 70)
+rt_hi = Raytracer(sun, spots, 200)
 occ = FastOccluder(sun, spots)
 
-renders = 5
-samples = 60
-T = np.linspace(0,period,samples)
+samples = 200
+T = np.linspace(0,2*period,samples)
 
 plt.clf()
-plt.subplot2grid((2,renders),(0,0), colspan=5)
-plt.plot(T, get_rvs(occ, T))
-plt.plot(T, get_rvs(rt, T))
 
-for i in range(renders):
-    plt.subplot2grid((2,renders),(1,i))
-    print "render %d/%d" % (i+1, renders)
-    t = 0.2*period*i/renders
-    rgb = rt.render( rt.trace(t) )
-    plt.axis('off')
-    plt.imshow(rgb, interpolation='nearest', origin='lower')
+plt.plot(T, get_rvs(rt, T), label="old (res=70)")
+plt.plot(T, get_rvs(rt_hi, T), label="old (res=200)")
+plt.plot(T, get_rvs(occ, T), label="new")
+
+plt.legend()
 
 plt.show()
