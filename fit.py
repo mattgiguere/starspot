b73890fe7a182f97883c169079e8324658854372
Cyril Zhang <cyril@mentalcanvas.com>
2014-04-28 16:15:45 -0400

    add transit trace demo

--- fit.py
+++ fit.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
+from scipy.optimize import leastsq
 from scipy.stats import linregress
 from starspot import *
 
@@ -14,30 +14,43 @@ RV = data[:,3]
 m, b, _, _, _ = linregress(T, RV)
 RV -= m*T + b
 
-def rv(inc, lat, phase, size):
+
+def rv(p):
+    inc, lat, phase, size = p
     star = RigidSphere(0.793 * 6.955e8, 34. * 86400., [0,math.cos(inc),math.sin(inc)])
     occ = FastOccluder(star, [star.spot(lat, phase, size)])
     return get_rvs(occ, T)
 
-res = 10
-n = 0
-best_params = None
-best_err = None
-for inc in np.linspace(-60, 60, res):
-    for lat in np.linspace(-90, 90, res):
-        for phase in np.linspace(0, 1, res):
-            for size in np.linspace(0, 0.01, res):
-                n += 1
-                print "try %d/%d" % (n, res**4)
-                r = rv(inc, lat, phase, size)
-                e = np.linalg.norm(r - RV)
-                if best_err == None or e < best_err:
-                    best_params = [inc, lat, phase, size]
-                    best_err = e
+# coarse search over parameter space to find a guess
+def guess_fit():
+    res = 12
+    n = 0
+    best_params = None
+    best_err = None
+    for inc in np.linspace(0, 90, res):
+        for lat in np.linspace(-inc, 90, res):
+            for phase in np.linspace(0, 1, res):
+                for size in np.linspace(0, 0.01, res):
+                    n += 1
+                    print "try %d/%d" % (n, res**4)
+                    p = [inc, lat, phase, size]
+                    r = rv(p)
+                    e = np.linalg.norm( r - RV )
+                    if best_err == None or e < best_err:
+                        best_params = p
+                        best_err = e
+    return best_params
 
-print best_params
+# use nonlinear optimization to refine fit
+def refine_fit(guess):
+    opt = leastsq(rv, guess)
+    return opt
+
+bp = guess_fit()
+print bp
 
 plt.clf()
-plt.scatter(T, RV)
-plt.plot(T, rv(*best_params))
+plt.scatter(T, RV + m*T + b)
+plt.plot(T, rv(bp) + m*T + b, label='guess')
+plt.legend()
 plt.show()
