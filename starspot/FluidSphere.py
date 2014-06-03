7b2e1c41159792aaf27b1283e54ee72f715ddacf
Aida Behmard <aida.behmard@yale.edu>
2014-06-03 13:51:28 -0400

    FluidSphere.py

---
+++ starspot/FluidSphere.py
@@ -0,0 +1,81 @@
+# Written by Aida Behmard, 6/3/2014
+# FluidSphere.py: fluid spherical model of a star
+# Accomadates differential angular velocities for spots ar different latitudes
+
+## Diff. rotation velocity notes ##
+# - Ref: "Living Reviews in Solar Physics"
+# - Donati  & Collier Cameron (1997):
+# - AB Doradus = K dwarf
+# - eq_rate = 12.2434 rad/d; diff_rate = 0.0564 rad/d
+
+import numpy as np
+import math
+import geometry
+import random
+
+class FluidSphere:
+    # Fluid sphere model of a star.
+    # A star is a sphere at (0,0,0). The observer looks in the -z direction.
+    # Rotation inclination is given by an angular velocity vector (numpy 3-array)
+
+
+    def rotation(eq_rate, pol_rate, theta):  # theta = lat 
+        diff_rate = eq_rate - pol_rate
+        rot_rate = eq_rate - diff_rate*math.sin(theta)**2
+        print "Rotation Rate", rot_rate
+
+        period = 2*math.pi / rot_rate
+        print "Rotation Period", period
+
+
+
+    def __init__(self, radius, period, axis):
+        # Constructor. Builds angular velocity vector using period and axis.
+        self.radius = radius
+        self.axis = np.array(axis)
+        self.axis = self.axis / np.linalg.norm(self.axis)
+        self.scalar_angvel = 2*math.pi / period
+        self.const_angvel = self.scalar_angvel * self.axis
+
+    def angvel(self, points):
+        # Takes points to angular velocities. Trivial constant function in this case.
+        return np.tile( self.const_angvel, (points.shape[0], 1) )
+
+    def max_radvel(self):
+        # Magnitude of RV at star's edge (accounting for inclination).
+        sini = math.sqrt( 1 - self.axis[2]**2 )
+        return self.scalar_angvel * self.radius * sini
+
+    def evolve(self, points, t):
+        # Given points at time 0, return points at time t.
+        mat = geometry.rotation_matrix(-self.scalar_angvel * t, self.axis)
+        return np.dot( points, mat.transpose() )
+
+    def occlude(self, pos, theta, points):
+        # Return a boolean mask of occluded points.
+        cosines = np.dot( points/self.radius, pos/self.radius )
+        return cosines >= math.cos(theta)
+
+
+
+    def spot(self, theta, phase, fracarea):
+        # Given spherical coords, get absolute coords
+        # Convention: theta is a latitude in degrees: 0 = equator, +90 = north pole
+        # Phase is in periods: 0 = transit, 0.5 = opposition, 1 = next transit
+
+        # find axis with z projection removed
+        w = self.axis[0:2] / np.sqrt(self.axis[0]**2 + self.axis[1]**2)
+        
+        # apply inclination
+        theta *= math.pi/180.0
+        theta -= math.asin(self.axis[2])
+
+        # get location at phase 0 (latitude only)
+        c, s = math.cos(theta), math.sin(theta)
+        p = self.radius * np.array( [s*w[0], s*w[1], c] )
+
+        # rotate to phase
+        mat = geometry.rotation_matrix(-2.0 * math.pi * phase, self.axis)
+        p = np.dot(p, mat.transpose())
+
+        return (p, geometry.cap_half_angle(fracarea))
