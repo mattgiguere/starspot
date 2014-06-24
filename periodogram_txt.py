02de64c1a5cb81031863322daf2b7b95b98fdff0
Aida Behmard <aida.behmard@yale.edu>
2014-06-24 16:57:30 -0400

    rotation periods for tau ceti-like stars

--- periodogram_txt.py
+++ periodogram_txt.py
@@ -11,11 +11,11 @@ from scipy.signal import lombscargle
 
 #------------------------------------------------------------
 # Load Data
-star = np.loadtxt('kepler_data/noisy_stars/10006542.txt')
+star = np.loadtxt('kepler_data/noisy_stars/11924268.txt')
 
 # generates 10000 ang. frequencies between 16240 and 16340
 nout = 1000.0
-f = np.linspace(0.2, 10.0, nout)
+f = np.linspace(0.07, 10.0, nout)
 
 t = star[:,1] 
 y = star[:,2] # corrected flux
